77b97a6df6c4dad89f1d55a4e0cd549efa771422
bj chang <bjchang@bjs-MacBook-Air.local>
2020-09-10 22:22:24 -0700

    Completed all tasks

--- index.js
+++ index.js
@@ -60,9 +60,23 @@ console.log(reviews[5].feedback);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
+function addReview(reviews){
 
+    return reviews.push({name: 'David', rating: 5, feedback:'Food was good and they have great taste in music.'});
+  
+}//function 
+console.log(addReview(reviews))//invoking function
 
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"*/
+
+function addReynaReview(reviews, index){
+
+    return reviews[index].feedback = 'this place is chill with really cool people, great for getting work done on weekdays'
+
+}//function
+console.log(addReynaReview(reviews, 7))//invoking function
+console.log(reviews)//verifying the value part of the key feedback in the object 'reyna' was changed.
+
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
@@ -77,6 +91,7 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
+
    return `${reviews[index].name} gave the restaurant a ${reviews[index].rating} star review and their feedback was: ${reviews[index].feedback}`
 
   }//function
@@ -92,9 +107,13 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
-  } 
+function getLastReview(reviews) {
+
+  return `${reviews[reviews.length - 1].name} gave the restaurant a ${reviews[reviews.length -1].rating} star review and their feedback was: ${reviews[reviews.length -1].feedback}`
+
+  }//function 
+
+  console.log(getLastReview(reviews))
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
